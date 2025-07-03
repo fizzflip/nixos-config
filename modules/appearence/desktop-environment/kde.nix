5c85ca20496d607df2a6e5f443a1770a23f3b323
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2025-07-03 14:48:41 +0530

    Replace sddm-astronaut-theme with SilentSDDM for KDE

--- modules/appearence/desktop-environment/kde.nix
+++ modules/appearence/desktop-environment/kde.nix
@@ -1,5 +1,10 @@
-{ pkgs, ... }:
-
+{ pkgs, inputs, ... }:
+let
+  sddm-theme = inputs.silentSDDM.packages.${pkgs.system}.default;
+  # .override {
+  #   theme = "rei";
+  # };
+in
 {
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = [ pkgs.kdePackages.plasma-browser-integration ];
@@ -12,14 +17,21 @@
 
     pkgs.python312Packages.kde-material-you-colors
 
+    sddm-theme
     pkgs.kdePackages.qtmultimedia
-    (pkgs.sddm-astronaut.override { embeddedTheme = "pixel_sakura"; })
   ];
 
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "sddm-astronaut-theme";
+    theme = sddm-theme.pname;
+    extraPackages = sddm-theme.propagatedBuildInputs;
+    settings = {
+      General = {
+        GreeterEnvironment = "QML2_IMPORT_PATH=${sddm-theme}/share/sddm/themes/${sddm-theme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
+        InputMethod = "qtvirtualkeyboard";
+      };
+    };
   };
 
   services.fwupd.enable = true;
