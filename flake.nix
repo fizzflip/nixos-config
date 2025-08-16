cf89e0f01a9c729a1d118b1cabfed45524389f02
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2025-08-17 01:46:49 +0530

    fix(sddm-theme): initial trial

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
