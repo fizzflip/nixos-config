1ea9b828f38e54fe1537048adafe716233d9aeb0
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2025-08-20 05:50:37 +0530

    chore: update flake.lock and sync branch with master

--- modules/appearence/components/sddm.nix
+++ modules/appearence/components/sddm.nix
@@ -1,27 +1,20 @@
-{ pkgs, inputs, ... }:
-let
-  sddm-theme = inputs.silentSDDM.packages.${pkgs.system}.default;
-  # .override {
-  #   theme = "rei";
-  # };
-in
-{
-  environment.systemPackages = [
-    sddm-theme
-    pkgs.kdePackages.qtmultimedia
-  ];
-
-  services.displayManager.sddm = {
-    enable = true;
-    package = pkgs.kdePackages.sddm;
-    wayland.enable = true;
-    theme = sddm-theme.pname;
-    extraPackages = sddm-theme.propagatedBuildInputs;
-    settings = {
-      General = {
-        GreeterEnvironment = "QML2_IMPORT_PATH=${sddm-theme}/share/sddm/themes/${sddm-theme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
-        InputMethod = "qtvirtualkeyboard";
+{ pkgs, inputs, ...}:
+  let sddm-theme = inputs.silentSDDM.packages.${pkgs.system}.default.override { theme = "ken"; }; in {
+   # include the test package which can be run using test-sddm-silent
+   environment.systemPackages = [ sddm-theme sddm-theme.test ];
+   qt.enable = true;
+   services.displayManager.sddm = {
+      package = pkgs.kdePackages.sddm; # use qt6 version of sddm
+      wayland.enable = true;
+      enable = true;
+      theme = sddm-theme.pname;
+      extraPackages = sddm-theme.propagatedBuildInputs;
+      settings = {
+        # required for styling the virtual keyboard
+        General = {
+          GreeterEnvironment = "QML2_IMPORT_PATH=${sddm-theme}/share/sddm/themes/${sddm-theme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
+          InputMethod = "qtvirtualkeyboard";
+        };
       };
-    };
-  };
+   };
 }
