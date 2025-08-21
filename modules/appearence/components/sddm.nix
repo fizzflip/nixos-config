59682d3dfe3f6546744ba5a21d93abfd74d4f654
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2025-08-21 07:01:22 +0530

    revert(sddm): to sddm-astronaut-theme

--- modules/appearence/components/sddm.nix
+++ modules/appearence/components/sddm.nix
@@ -1,20 +1,23 @@
-{ pkgs, inputs, ...}:
-  let sddm-theme = inputs.silentSDDM.packages.${pkgs.system}.default.override { theme = "ken"; }; in {
-   # include the test package which can be run using test-sddm-silent
-   environment.systemPackages = [ sddm-theme sddm-theme.test ];
-   qt.enable = true;
-   services.displayManager.sddm = {
-      package = pkgs.kdePackages.sddm; # use qt6 version of sddm
-      wayland.enable = true;
-      enable = true;
-      theme = sddm-theme.pname;
-      extraPackages = sddm-theme.propagatedBuildInputs;
-      settings = {
-        # required for styling the virtual keyboard
-        General = {
-          GreeterEnvironment = "QML2_IMPORT_PATH=${sddm-theme}/share/sddm/themes/${sddm-theme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
-          InputMethod = "qtvirtualkeyboard";
-        };
-      };
-   };
+{ pkgs, ... }:
+{
+  environment.systemPackages = [
+    pkgs.kdePackages.qtmultimedia
+   (pkgs.sddm-astronaut.override {
+      embeddedTheme = "pixel_sakura";
+   })
+  ];
+
+  services.displayManager.sddm = {
+    enable = true;
+    autoNumlock = true;
+    wayland.enable = true;
+    theme = "sddm-astronaut-theme";
+    package = pkgs.kdePackages.sddm;
+    extraPackages = [
+      pkgs.sddm-astronaut
+      pkgs.kdePackages.qtbase
+      pkgs.kdePackages.qtwayland
+      pkgs.kdePackages.qtmultimedia
+    ];
+  };
 }
