0a3625ae6f72fb74455ec1056f4730b28b6679a7
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2026-05-02 11:29:22 +0530

    chore: update flake inputs, optimize btrfs mount options, add godot, configure nix-ld libraries, and include UnityHub flatpak

--- modules/services/fhs-env.nix
+++ modules/services/fhs-env.nix
@@ -1,5 +1,32 @@
 { pkgs, ... }:
 {
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = [
+      pkgs.glib
+      pkgs.nss
+      pkgs.nspr
+      pkgs.dbus
+      pkgs.atk
+      pkgs.at-spi2-atk
+      pkgs.at-spi2-core
+      pkgs.libdrm
+      pkgs.expat
+      pkgs.libxkbcommon
+      pkgs.pango
+      pkgs.cairo
+      pkgs.alsa-lib
+      pkgs.libx11
+      pkgs.libxcomposite
+      pkgs.libxdamage
+      pkgs.libxext
+      pkgs.libxfixes
+      pkgs.libxrandr
+      pkgs.libxcb
+      pkgs.libgbm
+      pkgs.libGL
+      pkgs.stdenv.cc.cc.lib
+    ];
+  };
   environment.systemPackages = [ pkgs.steam-run ];
 }
