5e073373623dc320a3734a6bc2f881dabdb40981
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2026-05-06 03:21:17 +0530

    chore: switch default shell to nushell and update flake inputs

---
+++ modules/shell/nu.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    nushell
+    nushellPlugins.formats
+    nushellPlugins.gstat
+    nushellPlugins.highlight
+    nushellPlugins.query
+    nushellPlugins.polars
+    nushellPlugins.skim
+  ];
+
+  environment.shells = with pkgs; [
+    nushell
+  ];
+}
