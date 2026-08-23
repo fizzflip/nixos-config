0e9bf07939ee2193fb702cb2928a2d1750310804
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2026-08-23 13:08:34 +0530

    chore: disable bluetooth and update flakes

--- modules/services/bluetooth.nix
+++ modules/services/bluetooth.nix
@@ -1,7 +1,7 @@
 { ... }: {
   # Disabled by default — set enable = true to re-enable.
   hardware.bluetooth = {
-    enable = true;
+    enable = false;
     # powerOnBoot = true; # auto-power on at boot
     settings.General.Experimental = true; # shows battery % and extra BT info
   };
