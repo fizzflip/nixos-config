c02a07e13deb8c181ab966afb0e719b610bc78ea
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2026-08-30 19:05:08 +0530

    refactor: migrate cisco-packet-tracer to native nixpkgs fetching

--- modules/packages/lab/cisco-packet-tracer.nix
+++ modules/packages/lab/cisco-packet-tracer.nix
@@ -3,24 +3,14 @@
   lib,
   ...
 }:
-let
-  customPacketTracer = pkgs.ciscoPacketTracer9.override {
-    requireFile =
-      _:
-      pkgs.fetchurl {
-        url = "https://github.com/andknownmaly/packettracer/releases/download/9.0.0/CiscoPacketTracer_900_Ubuntu_64bit.deb";
-        sha256 = "086r5qbvvf8qarp554j4q4044vgswv3xfzv2iyvdqdzwqzac16nx";
-      };
-  };
-in
 {
-  environment.systemPackages = [ customPacketTracer ];
+  environment.systemPackages = [ pkgs.ciscoPacketTracer9 ];
   programs.firejail = {
     enable = true;
     wrappedBinaries = {
       packettracer9 = {
-        executable = "${lib.getExe customPacketTracer}";
-        desktop = "${customPacketTracer}/share/applications/cisco-packet-tracer-9.desktop";
+        executable = "${lib.getExe pkgs.ciscoPacketTracer9}";
+        desktop = "${pkgs.ciscoPacketTracer9}/share/applications/cisco-packet-tracer-9.desktop";
         extraArgs = [
           "--net=none"
           "--noprofile"
