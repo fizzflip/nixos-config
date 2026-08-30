34c20b9bfc52a75489e51cf53ea3fb0c5b02a6ef
Sandeep Prasad <175465964+fizzflip@users.noreply.github.com>
2026-08-30 20:24:51 +0530

    perf(laptop): optimize btrfs mount options and set compress-force=zstd:6

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -5,6 +5,16 @@
   modulesPath,
   ...
 }:
+let
+  btrfsCommonOptions = [
+    "compress-force=zstd:6"
+    "noatime"
+    "discard=async"
+    "ssd"
+    "space_cache=v2"
+    "commit=60"
+  ];
+in
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
@@ -27,40 +37,19 @@
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/0487551a-49fb-48d7-bbd2-841cfdf6d861";
     fsType = "btrfs";
-    options = [
-      "subvol=@root"
-      "compress-force=zstd:3"
-      "noatime"
-      "discard=async"
-      "ssd"
-      "commit=60"
-    ];
+    options = [ "subvol=@root" ] ++ btrfsCommonOptions;
   };
 
   fileSystems."/home" = {
     device = "/dev/disk/by-uuid/0487551a-49fb-48d7-bbd2-841cfdf6d861";
     fsType = "btrfs";
-    options = [
-      "subvol=@home"
-      "compress-force=zstd:3"
-      "noatime"
-      "discard=async"
-      "ssd"
-      "commit=60"
-    ];
+    options = [ "subvol=@home" ] ++ btrfsCommonOptions;
   };
 
   fileSystems."/nix" = {
     device = "/dev/disk/by-uuid/0487551a-49fb-48d7-bbd2-841cfdf6d861";
     fsType = "btrfs";
-    options = [
-      "subvol=@nix"
-      "compress-force=zstd:3"
-      "noatime"
-      "discard=async"
-      "ssd"
-      "commit=60"
-    ];
+    options = [ "subvol=@nix" ] ++ btrfsCommonOptions;
   };
 
   fileSystems."/boot" = {
